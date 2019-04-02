cf115cbb427123319451e252e3eb0ddbc8cd1c28
thevinter <n.brancatisanoev@studenti.unibs.it>
2019-04-02 18:43:50 +0200

    Tidied code

--- src/jraphics/Jraphics.java
+++ src/jraphics/Jraphics.java
@@ -11,7 +11,7 @@ public class Jraphics extends JPanel{
 	private static boolean isRunning = true;
 	public Mesh meshCube;
 	public Mat4x4 matProj;
-	private Jraphics panel;
+	public JFrame frame;
 	final static int TARGET_FPS = 60;
 	final static long OPTIMAL_TIME = 1000000000 / TARGET_FPS; 
 	private long delta = 0; 
@@ -20,34 +20,15 @@ public class Jraphics extends JPanel{
 	private long cTimeOld = 0;
 	private Vector3 vCamera = new Vector3(0,0,0);
 	private Vector3 vLookDir = new Vector3(0,0,1);
-	private ArrayList<Triangle> vecTrianglesToRaster = new ArrayList<>();
+	public ArrayList<Triangle> vecTrianglesToRaster = new ArrayList<Triangle>();
 	
 	//Override of the paintComponent method so it performs additional functions
 	@Override
 	public void paintComponent(Graphics g) {
+		
 		g.setColor(Color.BLACK);
 		
-		//For each triangle in the sorted list we draw them
-		for(Triangle tri : vecTrianglesToRaster) {
-				
-				//We store the points of the triangles in a temporary array so it's easier to work with them
-				int[] xPoints = {(int)tri.p.get(0).x,  (int)tri.p.get(1).x,  (int)tri.p.get(2).x};
-				int[] yPoints = {(int)tri.p.get(0).y, (int)tri.p.get(1).y, (int)tri.p.get(2).y};
-				
-				//We set the color that we calculated before
-				g.setColor(tri.color);
-				
-				//We create two polygons (one for the outline and one for the mesh)
-				Polygon t = new Polygon(xPoints,yPoints, 3);;
-				Polygon t_out = new Polygon(xPoints,yPoints, 3);
-				
-				//We draw them both and we paint the mesh one with our color and then we draw the outline
-				g.drawPolygon(t);
-				g .fillPolygon(t);
-				g.setColor(Color.BLACK);
-//				g.drawPolygon(t_out);		
-		}
-		
+		doGameUpdates(g);		
 		
 	}
 	
@@ -72,7 +53,7 @@ public class Jraphics extends JPanel{
 	}
 	
 	/*
-	 * Lots of auxiliary functions that are used to work with Vectors and Matrixes
+	 * Lot of auxiliary functions that are used to work with Vectors and Matrixes
 	 */
 	
 	//Sum of two Vectors
@@ -248,131 +229,157 @@ public class Jraphics extends JPanel{
 		return v;
 	}
 	
-	public void gameLoop() {
-		long lastLoopTime = System.nanoTime();
-		
-		while(isRunning) {
-			long now = System.nanoTime();
-			long updateLength = now - lastLoopTime;
-			lastLoopTime = now;
-			double delta = updateLength / ((double)OPTIMAL_TIME);
-			doGameUpdates(delta);
-			
-		}
+	private void doGameUpdates(Graphics g) {
+		//Creation of some rotational matrixes
+				Mat4x4 matRotZ = new Mat4x4();
+				Mat4x4 matRotX = new Mat4x4();
+				
+				matRotZ = MatrixRotationZ(fTheta * 0.5);
+				matRotX = MatrixRotationX(fTheta);
+				
+				//Creation of a translation matrix so our model isn't at 0,0,0
+				Mat4x4 matTrans ;
+				matTrans = MatrixMakeTranslation(0,0,5);
+				
+				//Creation of a world matrix that represents all the transformations of the objects
+				Mat4x4 matWorld;
+				matWorld = MatrixMakeIdentity();
+				matWorld = MatrixMultiplyMatrix(matRotZ, matRotX);
+				matWorld = MatrixMultiplyMatrix(matWorld, matTrans);
+				
+				//Vectors needed for the camera
+				Vector3 vUp = new Vector3(0,1,0);
+				Vector3 vTarget = VectorAdd(vCamera, vLookDir);
+				
+				Mat4x4 matCamera = MatrixPointAt(vCamera, vTarget, vUp);
+				
+				Mat4x4 matView = MatrixQuickInverse(matCamera);
+				
+				//Temp list to store triangles that we still have to sort
+				
+				vecTrianglesToRaster = new ArrayList<>();
+				//Loop that works on all of the triangles in our mesh
+				for(Triangle tri : meshCube.tris) {
+					
+					//Creation of some empty triangles needed for our transformation
+					Triangle triProjected = new Triangle(new Vector3(0,0,0), new Vector3(0,0,0), new Vector3(0,0,0));
+					Triangle triTransformed = new Triangle(new Vector3(0,0,0), new Vector3(0,0,0), new Vector3(0,0,0));
+					Triangle triViewed = new Triangle(new Vector3(0,0,0), new Vector3(0,0,0), new Vector3(0,0,0));
+					
+					//Translating all the vertexes of the tri using the world matrix
+					triTransformed.p.set(0, MatrixMultiplyVector(matWorld, tri.p.get(0)));
+					triTransformed.p.set(1, MatrixMultiplyVector(matWorld, tri.p.get(1)));
+					triTransformed.p.set(2, MatrixMultiplyVector(matWorld, tri.p.get(2)));
+					
+					//Creation of a normal and two lines
+					Vector3 normal = new Vector3(0,0,0); Vector3 line1 = new Vector3 (0,0,0); Vector3 line2 = new Vector3(0,0,0);
+					
+					//Auxialiary lines needed to calculate the normal of each triangle
+					line1 = VectorSub(triTransformed.p.get(1), triTransformed.p.get(0));
+					line2 = VectorSub(triTransformed.p.get(2), triTransformed.p.get(0));
+					
+					//Calculation of the normal using the cross product
+					normal = VectorCrossProduct(line1, line2);
+					
+					//Normalization of the normal
+					normal = VectorNormalize(normal);
+					
+					//A vector used by the camera
+					Vector3 vCameraRay = VectorSub(triTransformed.p.get(0), vCamera);
+			        
+					//If the projection of the normal on the view is lesser than 0 then we can actually see the triangle so we draw it
+			        if(VectorDotProduct(normal, vCameraRay) < 0) {
+							
+			        		//Auxiliary vector we use to hardcode the light direction (coming from the player)
+							Vector3 light_direction = new Vector3(0,1,-1);
+							
+							//We normalize it so we can work with unit vectors
+							light_direction = VectorNormalize(light_direction);
+							
+							//Here we calculate the dot product of the light and the normal so we can see how much the triangle deviates from our view
+							double dp = Math.max(0.1, VectorDotProduct(light_direction, normal));
+							
+							triViewed.p.set(0, MatrixMultiplyVector(matView, triTransformed.p.get(0)));
+							triViewed.p.set(1, MatrixMultiplyVector(matView, triTransformed.p.get(1)));
+							triViewed.p.set(2, MatrixMultiplyVector(matView, triTransformed.p.get(2)));
+
+							
+							//We project each triangle using the projection matrix
+							triProjected.p.set(0, MatrixMultiplyVector(matProj, triViewed.p.get(0)));
+							triProjected.p.set(1, MatrixMultiplyVector(matProj, triViewed.p.get(1)));
+							triProjected.p.set(2, MatrixMultiplyVector(matProj, triViewed.p.get(2)));
+							
+							//Extra math
+							triProjected.p.set(0, VectorDiv(triProjected.p.get(0), triProjected.p.get(0).w));
+							triProjected.p.set(1, VectorDiv(triProjected.p.get(1), triProjected.p.get(1).w));
+							triProjected.p.set(2, VectorDiv(triProjected.p.get(2), triProjected.p.get(2).w));
+							
+							//We offset the view by 1,1,0
+							Vector3 vOffsetView = new Vector3(1,1,0);
+							triProjected.p.set(0, VectorAdd(triProjected.p.get(0), vOffsetView));
+							triProjected.p.set(1, VectorAdd(triProjected.p.get(1), vOffsetView));
+							triProjected.p.set(2, VectorAdd(triProjected.p.get(2), vOffsetView));
+							
+							//We scale the coordinates based on our panel size
+							triProjected.p.get(0).x *= 0.5 * (double) frame.getSize().width;
+							triProjected.p.get(0).y *= 0.5 * (double) frame.getSize().height;
+							triProjected.p.get(1).x *= 0.5 * (double) frame.getSize().width;
+							triProjected.p.get(1).y *= 0.5 * (double) frame.getSize().height;
+							triProjected.p.get(2).x *= 0.5 * (double) frame.getSize().width;
+							triProjected.p.get(2).y *= 0.5 * (double) frame.getSize().height;
+							
+							//We create a temp triangle that is equivalent to the projected one so we can paint the inside
+							Triangle temp = new Triangle(triProjected.p.get(0), triProjected.p.get(1), triProjected.p.get(2));
+							
+							//We get the color based on the main color and the dot product of the normal and the light we calculated before
+							temp.color = getColor(Color.GREEN, 1-dp);
+							
+							//We add the triangle to the list we created before so we can sort it
+							vecTrianglesToRaster.add(temp);					
+						}									
+				}		
+				
+				//We sort the list so the triangles with the greatest Z get drawn last
+				Collections.sort(vecTrianglesToRaster, SortByZ.INSTANCE);		
+				
+				//For each triangle in the sorted list we draw them
+				for(Triangle tri : vecTrianglesToRaster) {
+						
+						//We store the points of the triangles in a temporary array so it's easier to work with them
+						int[] xPoints = {(int)tri.p.get(0).x,  (int)tri.p.get(1).x,  (int)tri.p.get(2).x};
+						int[] yPoints = {(int)tri.p.get(0).y, (int)tri.p.get(1).y, (int)tri.p.get(2).y};
+						
+						//We set the color that we calculated before
+						g.setColor(tri.color);
+						
+						//We create two polygons (one for the outline and one for the mesh)
+						Polygon t = new Polygon(xPoints,yPoints, 3);;
+						Polygon t_out = new Polygon(xPoints,yPoints, 3);
+						
+						//We draw them both and we paint the mesh one with our color and then we draw the outline
+						g.drawPolygon(t);
+						g .fillPolygon(t);
+						g.setColor(Color.BLACK);
+//						g.drawPolygon(t_out);		
+				}
 	}
 	
-	private void doGameUpdates(double delta) {
+	public void gameLoop() {
 		
-		//Creation of some rotational matrixes
-		Mat4x4 matRotZ = new Mat4x4();
-		Mat4x4 matRotX = new Mat4x4();
-		
-		matRotZ = MatrixRotationZ(delta * 0.5);
-		matRotX = MatrixRotationX(delta);
-		
-		//Creation of a translation matrix so our model isn't at 0,0,0
-		Mat4x4 matTrans ;
-		matTrans = MatrixMakeTranslation(0,0,100);
-		
-		//Creation of a world matrix that represents all the transformations of the objects
-		Mat4x4 matWorld;
-		matWorld = MatrixMakeIdentity();
-		matWorld = MatrixMultiplyMatrix(matRotZ, matRotX);
-		matWorld = MatrixMultiplyMatrix(matWorld, matTrans);
-		
-		//Vectors needed for the camera
-		Vector3 vUp = new Vector3(0,1,0);
-		Vector3 vTarget = VectorAdd(vCamera, vLookDir);
-		
-		Mat4x4 matCamera = MatrixPointAt(vCamera, vTarget, vUp);
-		
-		Mat4x4 matView = MatrixQuickInverse(matCamera);
-		
-		//Temp list to store triangles that we still have to sort
-		ArrayList<Triangle> vecTrianglesToRaster = new ArrayList<>();
-	
-		//Loop that works on all of the triangles in our mesh
-		for(Triangle tri : meshCube.tris) {
-			
-			//Creation of some empty triangles needed for our transformation
-			Triangle triProjected = new Triangle(new Vector3(0,0,0), new Vector3(0,0,0), new Vector3(0,0,0));
-			Triangle triTransformed = new Triangle(new Vector3(0,0,0), new Vector3(0,0,0), new Vector3(0,0,0));
-			Triangle triViewed = new Triangle(new Vector3(0,0,0), new Vector3(0,0,0), new Vector3(0,0,0));
-			
-			//Translating all the vertexes of the tri using the world matrix
-			triTransformed.p.set(0, MatrixMultiplyVector(matWorld, tri.p.get(0)));
-			triTransformed.p.set(1, MatrixMultiplyVector(matWorld, tri.p.get(1)));
-			triTransformed.p.set(2, MatrixMultiplyVector(matWorld, tri.p.get(2)));
-			
-			//Creation of a normal and two lines
-			Vector3 normal = new Vector3(0,0,0); Vector3 line1 = new Vector3 (0,0,0); Vector3 line2 = new Vector3(0,0,0);
-			
-			//Auxialiary lines needed to calculate the normal of each triangle
-			line1 = VectorSub(triTransformed.p.get(1), triTransformed.p.get(0));
-			line2 = VectorSub(triTransformed.p.get(2), triTransformed.p.get(0));
-			
-			//Calculation of the normal using the cross product
-			normal = VectorCrossProduct(line1, line2);
-			
-			//Normalization of the normal
-			normal = VectorNormalize(normal);
-			
-			//A vector used by the camera
-			Vector3 vCameraRay = VectorSub(triTransformed.p.get(0), vCamera);
-	        
-			//If the projection of the normal on the view is lesser than 0 then we can actually see the triangle so we draw it
-	        if(VectorDotProduct(normal, vCameraRay) < 0) {
-					
-	        		//Auxiliary vector we use to hardcode the light direction (coming from the player)
-					Vector3 light_direction = new Vector3(0,1,-1);
-					
-					//We normalize it so we can work with unit vectors
-					light_direction = VectorNormalize(light_direction);
-					
-					//Here we calculate the dot product of the light and the normal so we can see how much the triangle deviates from our view
-					double dp = Math.max(0.1, VectorDotProduct(light_direction, normal));
-					
-					triViewed.p.set(0, MatrixMultiplyVector(matView, triTransformed.p.get(0)));
-					triViewed.p.set(1, MatrixMultiplyVector(matView, triTransformed.p.get(1)));
-					triViewed.p.set(2, MatrixMultiplyVector(matView, triTransformed.p.get(2)));
-
-					
-					//We project each triangle using the projection matrix
-					triProjected.p.set(0, MatrixMultiplyVector(matProj, triViewed.p.get(0)));
-					triProjected.p.set(1, MatrixMultiplyVector(matProj, triViewed.p.get(1)));
-					triProjected.p.set(2, MatrixMultiplyVector(matProj, triViewed.p.get(2)));
-					
-					//Extra math
-					triProjected.p.set(0, VectorDiv(triProjected.p.get(0), triProjected.p.get(0).w));
-					triProjected.p.set(1, VectorDiv(triProjected.p.get(1), triProjected.p.get(1).w));
-					triProjected.p.set(2, VectorDiv(triProjected.p.get(2), triProjected.p.get(2).w));
-					
-					//We offset the view by 1,1,0
-					Vector3 vOffsetView = new Vector3(1,1,0);
-					triProjected.p.set(0, VectorAdd(triProjected.p.get(0), vOffsetView));
-					triProjected.p.set(1, VectorAdd(triProjected.p.get(1), vOffsetView));
-					triProjected.p.set(2, VectorAdd(triProjected.p.get(2), vOffsetView));
-					
-					//We scale the coordinates based on our panel size
-					triProjected.p.get(0).x *= 0.5 * (double) panel.getSize().width;
-					triProjected.p.get(0).y *= 0.5 * (double) panel.getSize().height;
-					triProjected.p.get(1).x *= 0.5 * (double) panel.getSize().width;
-					triProjected.p.get(1).y *= 0.5 * (double) panel.getSize().height;
-					triProjected.p.get(2).x *= 0.5 * (double) panel.getSize().width;
-					triProjected.p.get(2).y *= 0.5 * (double) panel.getSize().height;
-					
-					//We create a temp triangle that is equivalent to the projected one so we can paint the inside
-					Triangle temp = new Triangle(triProjected.p.get(0), triProjected.p.get(1), triProjected.p.get(2));
-					
-					//We get the color based on the main color and the dot product of the normal and the light we calculated before
-					temp.color = getColor(Color.GREEN, 1-dp);
-					
-					//We add the triangle to the list we created before so we can sort it
-					vecTrianglesToRaster.add(temp);					
-				}									
-		}		
-		
-		//We sort the list so the triangles with the greatest Z get drawn last
-		Collections.sort(vecTrianglesToRaster, SortByZ.INSTANCE);
+		while(isRunning) {
+			frame.repaint();
+			long cTimeNow = System.currentTimeMillis();
+			if(cTimeOld != 0)
+				fElapsedTime = (double)((cTimeNow - cTimeOld) * 0.001);
+		    fTheta += fElapsedTime;
+		    cTimeOld = cTimeNow;
+		    frame.repaint();
+		    try {
+		    	Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 }
